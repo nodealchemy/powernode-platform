24d7f5454cd53390f37674b9a97f873c93defcba
Everett C. Haimes III <everett@quickfreeze.net>
2026-03-30 03:22:17 -0800

    fix(worker): explicitly require AiWorkflowErrorTrackingService in boot (no autoloader)

--- worker/config/boot.rb
+++ worker/config/boot.rb
@@ -34,6 +34,7 @@ require_relative '../app/services/credential_resolver'
 # Auto-require external service integrations
 require_relative '../app/services/firebase_service'
 require_relative '../app/services/twilio_service'
+require_relative '../app/services/ai_workflow_error_tracking_service'
 
 # Require base job first
 require_relative '../app/jobs/base_job'
